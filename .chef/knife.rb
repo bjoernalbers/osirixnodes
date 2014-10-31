352a96c60b9f6b9f2ae6b2b61d943c9d94ad5db2
Björn Albers <bjoernalbers@gmail.com>
2014-10-31 22:33:30 +0100

    Fix SSL warning from chef.

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -6,3 +6,7 @@ data_bag_path    "data_bags"
 #encrypted_data_bag_secret "data_bag_key"
 
 knife[:berkshelf_path] = "cookbooks"
+
+# Fix SSL warning from chef
+# (http://stackoverflow.com/questions/22991561/chef-solo-ssl-warning-when-provisioning)
+ssl_verify_mode :verify_peer
