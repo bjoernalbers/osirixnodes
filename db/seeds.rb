7ae3efec6fc1970f40c2886d441361ccea7039f0
Björn Albers <bjoernalbers@gmail.com>
2014-11-21 21:19:53 +0100

    Seed database with example nodelist.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+user = User.find_or_initialize_by(api_key: 'example')
+user.update!(email:    'example@osirixnodes.com',
+             password: Rails.application.secrets.example_user_password)
 
-user = User.create!(email: 'hallo@osirixnodes.com',
-                    password: 'Surfer"Dolls90',
-                    api_key: 'example')
-user.nodes.create!(name: 'OsiriXNodes.com',
-                   host: '127.0.0.1',
-                   port: 11112,
-                   aetitle: 'OSIRIXNODES')
-
+node = user.nodes.first_or_initialize
+node.update!(name:    'Example from OsiriXNodes.com',
+             aetitle: 'EXAMPLE',
+             address: '127.0.0.1',
+             port:    11112)
---
+++ spec/tasks/db_seed_spec.rb
@@ -0,0 +1,58 @@
+require 'spec_helper'
+require 'rake'
+
+Osirixnodes::Application.load_tasks
+
+describe 'db:seed' do
+  # User
+  let(:api_key) { 'example' } # https://osirixnodes.com/plist/example
+  let(:email)   { 'example@osirixnodes.com' }
+
+  # Node
+  let(:name)    { 'Example from OsiriXNodes.com' }
+  let(:aetitle) { 'EXAMPLE' }
+  let(:address) { '127.0.0.1' }
+  let(:port)    { 11112 }
+
+  let(:user) { User.find_by(api_key: api_key) }
+  let(:node) { user.nodes.first }
+
+  def run_task
+    Rake::Task['db:seed'].invoke
+    Rake::Task['db:seed'].reenable # Otherwise the task would run only once.
+  end
+
+  shared_examples_for 'db:seed' do
+    it 'creates example user' do
+      expect(user.email).to eq(email)
+      password = Rails.application.secrets.example_user_password
+      expect(user.valid_password?(password)).to be true
+    end
+
+    it 'creates example node' do
+      expect(node.name).to    eq(name)
+      expect(node.aetitle).to eq(aetitle)
+      expect(node.address).to eq(address)
+      expect(node.port).to    eq(port)
+    end
+  end
+
+  context 'with unseeded database' do
+    before do
+      run_task
+    end
+    
+    it_behaves_like 'db:seed'
+  end
+
+  context 'with seeded database' do
+    before do
+      # Seed database with wrong example user and node.
+      user = create(:user, api_key: api_key)
+      node = create(:node, user: user)
+      run_task
+    end
+
+    it_behaves_like 'db:seed'
+  end
+end
