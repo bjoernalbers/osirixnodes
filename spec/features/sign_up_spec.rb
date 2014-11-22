d7ca379d08a988f9f709e37458960662dc178e8e
Björn Albers <bjoernalbers@gmail.com>
2014-11-23 00:40:52 +0100

    Style the app.
    - add bootstrap customizations
    - update devise views

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -1,19 +1,19 @@
 # All users have to sign up
 # Because the business owner needs the email address
 
-feature 'Sign Up' do
+feature 'Sign up' do
   let(:email) { 'steve@jobs.com' }
   let(:password) { '&Minted14Jodi' }
 
-  scenario 'happy path' do
+  scenario 'with valid attributes' do
     visit '/'
+
     click_link 'Sign up'
-    fill_in 'Email', with: email
-    fill_in 'Password', with: password
+    fill_in 'user_email', with: email
+    fill_in 'user_password', with: password
     click_button 'Sign up'
 
     expect(page).to have_content(/confirmation/i)
-
     expect(current_path).to eq '/'
   end
 end
