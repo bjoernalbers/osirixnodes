eb2c9f825338e588218d1f2554c9f9ccce74d29a
Björn Albers <bjoernalbers@gmail.com>
2014-12-27 12:00:23 +0100

    Decrease Unicorn workers from 5 to 4
    ...because we got some out of memory errors in production:
    
      E, [2014-12-23T07:41:25.036177 #28797] ERROR -- : Cannot allocate memory
        - fork(2) (Errno::ENOMEM)

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,5 +5,5 @@ set :full_app_name, "#{fetch(:application)}_#{fetch(:stage)}"
 server 'osirixnodes.com', user: 'deploy', roles: %w{web app db}, primary: true
 set :deploy_to, "/home/#{fetch(:deploy_user)}/apps/#{fetch(:full_app_name)}"
 set :rails_env, :production
-set :unicorn_worker_count, 5
+set :unicorn_worker_count, 4
 set :enable_ssl, true
