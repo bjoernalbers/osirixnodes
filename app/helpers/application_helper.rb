1626ed42d0548855ae03610f04c61a3d2ccff3ec
Björn Albers <bjoernalbers@gmail.com>
2015-07-28 14:15:34 +0200

    Remove static pages for online presentation.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,2 @@
 module ApplicationHelper
-  def support_email(text = 'bjoernalbers@gmail.com')
-    link_to(text, 'mailto:bjoernalbers@gmail.com')
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'pages/index'
-  get 'pages/about'
-  get 'pages/privacy'
 
   authenticated :user do
     root to: 'nodes#index', as: :user_root
   end
-  root to: 'pages#index'
+  # root to: ???
   resources :nodes, except: [:show]
   resources :plist, only: :show
 end
