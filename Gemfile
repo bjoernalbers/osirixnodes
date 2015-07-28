734948a41c154da85d1da7367802980982fb90b1
Björn Albers <bjoernalbers@gmail.com>
2015-07-28 11:59:59 +0200

    Update Ruby and Gems for local deployment.
    We wanna deploy on OS X rather than Heroku:
    
    - switch to Ruby 2.0.0 (default Ruby on recent versions of OS X)
    - remove Heroku-related gems
    - switch from PostgreSQL to SQLite which should work for most
      deployments
    
    Upgrades some gems (Rails 4.2.3, etc.)

--- Gemfile
+++ Gemfile
@@ -1,21 +1,20 @@
 source 'https://rubygems.org'
 
-ruby '2.1.2'
+ruby '2.0.0'
 
 # Heroku
-gem 'puma', '~> 2.11'
-gem 'pg', '~> 0.18'
-gem 'rails_12factor', group: :production
-gem 'rack-timeout'
+gem 'puma', '~> 2.12'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '4.1.7'
+gem 'rails', '4.2.3'
+# Use sqlite3 as the database for Active Record
+gem 'sqlite3'
 # Use SCSS for stylesheets
-gem 'sass-rails', '~> 4.0.3'
+gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .js.coffee assets and views
-gem 'coffee-rails', '~> 4.0.0'
+gem 'coffee-rails', '~> 4.1.0'
 # See https://github.com/sstephenson/execjs#readme for more supported runtimes
 # gem 'therubyracer',  platforms: :ruby
 
@@ -51,7 +50,7 @@ group :development do
 end
 
 group :development, :test do
-  gem 'rspec-rails', '~> 3.0.0'
+  gem 'rspec-rails', '~> 3.3'
   gem 'faker', '~> 1.4'
   gem 'capybara', '~> 2.4'
   gem 'database_cleaner', '~> 1.3'
--- db/schema.rb
+++ db/schema.rb
@@ -13,46 +13,43 @@
 
 ActiveRecord::Schema.define(version: 20141219171449) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "nodes", force: true do |t|
-    t.string   "name"
-    t.string   "address"
+  create_table "nodes", force: :cascade do |t|
+    t.string   "name",               limit: 255
+    t.string   "address",            limit: 255
     t.integer  "port"
-    t.string   "aetitle"
+    t.string   "aetitle",            limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "user_id",                        null: false
+    t.integer  "user_id",                                    null: false
     t.integer  "transfer_syntax_id"
-    t.integer  "transfer_syntax",    default: 0, null: false
-    t.integer  "retrieve_mode",      default: 0, null: false
+    t.integer  "transfer_syntax",                default: 0, null: false
+    t.integer  "retrieve_mode",                  default: 0, null: false
   end
 
-  create_table "users", force: true do |t|
-    t.string   "email"
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  limit: 255
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",                      default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "api_key",                             null: false
-    t.string   "confirmation_token"
+    t.string   "api_key",                limit: 255,              null: false
+    t.string   "confirmation_token",     limit: 255
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email"
+    t.string   "unconfirmed_email",      limit: 255
     t.boolean  "guest"
   end
 
-  add_index "users", ["api_key"], name: "index_users_on_api_key", unique: true, using: :btree
-  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["api_key"], name: "index_users_on_api_key", unique: true
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
