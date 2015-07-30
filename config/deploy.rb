249444ccf0fc98848bdb86ebf9cbb682859b7918
Björn Albers <bjoernalbers@gmail.com>
2015-07-30 12:22:23 +0200

    Add capistrano setup for OS X deployment.

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,10 +23,16 @@ set :repo_url, 'git@github.com:bjoernalbers/osirixnodes.git'
 # set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml',
+                                                 'config/secrets.yml',
+                                                 'db/production.sqlite3')
 
 # Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('log',
+                                               'tmp/pids',
+                                               'tmp/cache',
+                                               'tmp/sockets',
+                                               'vendor/bundle')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
@@ -34,15 +40,16 @@ set :repo_url, 'git@github.com:bjoernalbers/osirixnodes.git'
 # Default value for keep_releases is 5
 # set :keep_releases, 5
 
-namespace :deploy do
+set :services, %w(app)
 
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
+namespace :deploy do
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      invoke 'launchd:unload'
+      invoke 'launchd:load'
     end
   end
 
+  after :publishing, :restart
 end
