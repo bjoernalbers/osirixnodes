4593acd00db530bb3753d85bfaf17a65b1adaea1
Björn Albers <bjoernalbers@gmail.com>
2018-01-18 16:21:14 +0100

    Refactor request specs.
    Just a little renaming.

--- spec/requests/nodes_spec.rb
+++ spec/requests/nodes_spec.rb
@@ -22,19 +22,19 @@ describe 'GET /nodes.plist' do
     expect(plist_response.count).to eq(1)
   end
 
-  it 'includes node names' do
+  it 'includes Description' do
     expect(first_node['Description']).to eq(node.name)
   end
 
-  it 'includes node aetitles' do
+  it 'includes AETitle' do
     expect(first_node['AETitle']).to eq(node.aetitle)
   end
 
-  it 'includes node addresses' do
+  it 'includes Address' do
     expect(first_node['Address']).to eq(node.address)
   end
 
-  it 'includes node ports' do
+  it 'includes Port' do
     expect(first_node['Port']).to eq(node.port)
   end
 
@@ -50,7 +50,7 @@ describe 'GET /nodes.plist' do
     expect(first_node['TLSEnabled']).to eq false
   end
 
-  it 'includes node transfer syntax' do
+  it 'includes TransferSyntax' do
     expect(first_node['TransferSyntax']).to eq(node.transfer_syntax)
   end
 
@@ -58,7 +58,7 @@ describe 'GET /nodes.plist' do
     expect(first_node['Type']).to eq 'DICOM'
   end
 
-  it 'includes node retrieve mode' do
+  it 'includes retrieveMode' do
     expect(first_node['retrieveMode']).to eq(node.retrieve_mode)
   end
 end
