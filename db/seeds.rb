48644e6da26b352c3d0cebc651e2c16052147806
Björn Albers <bjoernalbers@gmail.com>
2021-02-02 09:06:19 +0100

    Only seed database when no nodes are present.
    This prevents that `rake db:seed` changes an existing node.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
 # Example Node
-node = Node.first_or_initialize
-node.update!(name:    'A boring example node',
-             aetitle: 'OSIRIXNODES',
-             address: '127.0.0.1',
-             port:    11112,
-             transfer_syntax: 0)
+if Node.count.zero?
+  Node.create!(name:            'A boring example node',
+               aetitle:         'OSIRIXNODES',
+               address:         '127.0.0.1',
+               port:            11112,
+               transfer_syntax: 0)
+end
--- spec/tasks/db_seed_spec.rb
+++ spec/tasks/db_seed_spec.rb
@@ -12,35 +12,14 @@ describe 'db:seed' do
 
   let(:node) { Node.last }
 
-  def run_task
+  before do
     Rake::Task['db:seed'].invoke
-    Rake::Task['db:seed'].reenable # Otherwise the task would run only once.
   end
-
-  shared_examples_for 'db:seed' do
-    it 'creates example node' do
-      expect(node.name).to    eq(name)
-      expect(node.aetitle).to eq(aetitle)
-      expect(node.address).to eq(address)
-      expect(node.port).to    eq(port)
-    end
-  end
-
-  context 'with unseeded database' do
-    before do
-      run_task
-    end
     
-    it_behaves_like 'db:seed'
-  end
-
-  context 'with seeded database' do
-    before do
-      # Seed database with wrong example node.
-      node = create(:node)
-      run_task
-    end
-
-    it_behaves_like 'db:seed'
+  it 'creates example node' do
+    expect(node.name).to    eq(name)
+    expect(node.aetitle).to eq(aetitle)
+    expect(node.address).to eq(address)
+    expect(node.port).to    eq(port)
   end
 end
